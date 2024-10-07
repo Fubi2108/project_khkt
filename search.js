b9263a10a46cfda98298a4f27729e633420c4e77
unknown <doanbaongoc2108@gmail.com>
2024-10-07 18:34:24 +0700

    basic search

--- search.js
+++ search.js
@@ -1,8 +1,17 @@
+function find_word(word){
+    fetch("lessons/testing.json")
+    .then(res => res.json())
+    .then(res => res.forEach(cword => {
+        if(cword.name.includes(word))
+            console.log(cword.name);
+    }))
+}
+
 const searchin = document.getElementById("searchbar");
 
 searchin.addEventListener("keydown", function(event){
     if(event.key == "Enter"){
         event.preventDefault();
-        console.log(this.value);
+        find_word(this.value);
     }
 })
